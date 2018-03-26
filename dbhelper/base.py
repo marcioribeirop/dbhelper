35004c9122c4e2141ea017886745ca62db067126
marciorp <marcio.ribeiro@tevec.com.br>
2018-03-26 16:29:55 -0300

    Corrected base

--- dbhelper/base.py
+++ dbhelper/base.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://tevec:ew3wrnaKRy@6D3fyGGtvPGL6tr@172.26.3.122:3306/microservices")
+engine = create_engine("mysql+pymysql://tevec:ew3wrnaKRy@6D3fyGGtvPGL6tr@172.31.41.158:3306/microservices")
 
 Session = sessionmaker(bind=engine)
 
